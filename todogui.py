de0a50760f70c2a61538dec75e613cc2ace9f817
bytebandit,viron <mindedbusines@gmail.com>
2026-01-29 16:46:01 +0000

    todo limiter added ,reset added ,back to welcome removed

--- todogui.py
+++ todogui.py
@@ -5,7 +5,7 @@ todos = []
 
 root = tk.Tk()
 root.title("Todo App")
-root.geometry("400x500")
+root.geometry("400x600")
 
 #switcher
 def switch_to_main():
@@ -41,10 +41,10 @@ td_total.pack(padx=10, pady=10)
 
 
 todo_display = tk.Frame(main_page)
-todo_display.pack(pady=10)
+todo_display.pack(pady=4)
 
 for todo in todos:
-    todo_frame = tk.Frame(todo_display, relief=tk.RIDGE, borderwidth=1, padx=10, pady=5)
+    todo_frame = tk.Frame(todo_display, relief=tk.RIDGE, borderwidth=1, padx=4, pady=5)
     todo_frame.pack(fill="x", padx=10, pady=5)
     td_item = tk.Label(todo_frame, text=f'Task: {todo["text"]}', font=("Arial", 12))
     td_item.pack(anchor="w")
@@ -54,7 +54,7 @@ for todo in todos:
 
 # nt
 input_frame = tk.Frame(main_page)
-input_frame.pack(pady=20)
+input_frame.place(rely=0.8,relx=0.5,anchor=tk.CENTER )
 
 tk.Label(input_frame, text="Add New Todo:").pack()
 todo_entry = tk.Entry(input_frame, width=30)
@@ -72,6 +72,7 @@ def add_todo():
         todo_entry.delete(0, tk.END)
         messagebox.askokcancel("Success", "Todo added!")
         refresh_todos()
+        todoLimiter()
     else:
         messagebox.showwarning("Empty", "Please enter a todo!")
 
@@ -79,7 +80,7 @@ def refresh_todos():
     for widget in todo_display.winfo_children():
         widget.destroy()
     for todo in todos:
-        todo_frame = tk.Frame(todo_display, relief=tk.RIDGE, borderwidth=2, padx=10, pady=5)
+        todo_frame = tk.Frame(todo_display, relief=tk.RIDGE, borderwidth=1, padx=10, pady=5)
         todo_frame.pack(fill="x", padx=10, pady=5)
         td_item = tk.Label(todo_frame, text=f'Task: {todo["text"]}', font=("Arial", 12))
         td_item.pack(anchor="w")
@@ -87,7 +88,20 @@ def refresh_todos():
         td_status = tk.Label(todo_frame, text=f'Status: {status_text}')
         td_status.pack(anchor="w")
     td_total.config(text=f'Number Of Todos: {len(todos)}')
-tk.Button(input_frame, text="Add Todo", command=add_todo, width=20).pack(pady=2)
-tk.Button(main_page, text="Back to Welcome",command=switch_to_welcome, width=20).pack(pady=4)
 
+def todoLimiter():
+    if len(todos) >= 2:
+        todo_btnsADD.pack_forget()
+        messagebox.showwarning("ex0001","limit of 5 todos reached")
+def todo_deleter():
+    for todo in todos:
+        todos.pop()
+    refresh_todos()
+    todo_btnsADD.pack()
+
+todo_btnsADD = tk.Button(input_frame, text="Add Todo", command=add_todo, width=20)
+todo_btnsADD.pack(pady=2)
+
+todo_reset = tk.Button(input_frame, text="reset",width=20 , command=todo_deleter)
+todo_reset.pack(side=tk.BOTTOM)
 root.mainloop()
